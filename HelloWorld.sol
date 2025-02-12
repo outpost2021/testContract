9b9606745f67956176a3d700bae55dab1f8554e6
outpost2021 <outpost202101@gmail.com>
2025-02-12 21:18:14 +0800

    test

---
+++ HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT 
+// compiler version must be greater than or equal to 0.8.26 and less than 0.9.0 
+pragma solidity ^0.8.26;
+
+contract HelloWorld { 
+    string public greeting; // declare variable first
+
+   constructor() {
+       greeting = "Hello World!";
+   }
+}
